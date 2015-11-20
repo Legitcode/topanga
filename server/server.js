e8b15021329b046105be22b32e8411d15d29ca59
Zach Silveira <zackify@gmail.com>
2015-11-19 22:47:08 -0500

    server basicly here

---
+++ server/server.js
@@ -0,0 +1,18 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(6666);
+
+function handler (req, res) {
+  res.writeHead(200);
+  res.end('hi');
+}
+
+io.on('connection', socket => {
+  socket.emit('welcome', 'you are connected to topanga >:)')
+  socket.on('username', username => socket.username = username)
+
+  socket.on('message', message => socket.broadcast.emit('message', message))
+});
+console.log('topanga running...')
